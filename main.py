7fc36de75a06e7b0a1c2d9a956726a6c942869de
themrinch <136446022+themrinch@users.noreply.github.com>
2024-05-23 18:02:02 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+import json
+
+
+class Item(BaseModel):
+    text: str
+
+
+app = FastAPI()
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+labels = ['Visa', 'Accommodation', 'Studies']
+
+with open('data.json', 'r') as file:
+    answers = json.load(file)
+
+
+@app.get("/")
+async def root():
+    return {'model': 'facebook/bart-large-mnli'}
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    """Text Classifier"""
+    classified = classifier(item.text, labels)["labels"][0]
+    return answers[classified]
