e878104506d9b0db8ef4c9f04abffac7e189f46b
themrinch <136446022+themrinch@users.noreply.github.com>
2024-05-23 20:32:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ class Item(BaseModel):
 
 
 app = FastAPI()
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+classifier = pipeline("zero-shot-classification",
+                      model="facebook/bart-large-mnli")
 labels = ['Visa', 'Accommodation', 'Studies']
 
 with open('data.json', 'r') as file:
