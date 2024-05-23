44d4bcae1a6ca8d49f805a9dfc4715899bb1639b
themrinch <136446022+themrinch@users.noreply.github.com>
2024-05-23 21:46:22 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -15,7 +15,9 @@ def test_read_main():
 def test_predict_answer():
     response = client.post("/predict/",
                            json={"text": "I applied for a visa invitation"
-                           "a few days/weeks ago and still haven't received"
-                           "it. What should I do in this situation?"})
+                                 "a few days/weeks ago and still haven't received"
+                                 "it. What should I do in this situation?"})
     assert response.status_code == 200
-    assert response.json() == json.load(open('data.json', 'r', encoding='utf-8'))['Visa']
+    assert response.json() == json.load(open('data.json', 
+                                             'r',
+                                             encoding='utf-8'))['Visa']
