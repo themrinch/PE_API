54befe236dff84b9203505e2de76287840a5efc3
themrinch <136446022+themrinch@users.noreply.github.com>
2024-05-23 21:34:12 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
+import json
+
 
 client = TestClient(app)
 
@@ -12,26 +14,9 @@ def test_read_main():
 
 def test_predict_answer():
     response = client.post("/predict/",
-                           json={"text": '''I applied for a visa invitation 
-                           a few days/weeks 
+                           json={"text": '''I applied for a visa invitation
+                           a few days/weeks
                            ago and still haven't received it.
                            What should I do in this situation?'''})
     assert response.status_code == 200
-    assert response.json() == '''I applied for a visa invitation 
-    a few days/weeks ago and still 
-    haven't received it. 
-    What should I do in this situation?
-    There is nothing to worry about. 
-    The invitation is issued by the Ministry 
-    of Foreign Affairs of the Russian Federation and takes 
-    at least 30 days to be prepared. 
-    If you do not receive your invitation 40 days
-    after submitting the application, please
-    contact the Admissions Office at admission@urfu.ru.
-    
-    How do I extend my visa?
-    Please visit the GUK-109 office
-    at least six weeks prior to the
-    expiration date of your current visa.
-    You will be consulted on the vis
-    a extension procedures.'''
+    assert response.json() == json.load(open('data.json', 'r', encoding='utf-8'))['Visa']
