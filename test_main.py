eba21828051e3aa5a8feedc96113d163f3dd4b46
themrinch <136446022+themrinch@users.noreply.github.com>
2024-05-25 16:25:45 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -21,4 +21,5 @@ def test_predict_answer():
     assert response.status_code == 200
     assert response.json() == json.load(open('data.json',
                                              'r',
-                                             encoding='utf-8'))['Visa invitation']
+                                             encoding='utf-8')
+                                       )['Visa invitation']
